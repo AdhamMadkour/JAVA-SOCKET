59eb37075ab6fa5325e055f5caeae4a06857ef9a
AdhamMadkour <64507869+AdhamMadkour@users.noreply.github.com>
2021-11-02 09:01:39 +0200

    Add files via upload

---
+++ Client.java
@@ -0,0 +1,51 @@
+import java.io.*;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Objects;
+import java.util.Scanner;
+
+public class Client {
+    public static void main(String[] args) {
+        Socket socket = null;
+        InputStreamReader inputStreamReader = null;
+        OutputStreamWriter outputStreamWriter = null;
+        BufferedReader bufferedReader = null;
+        BufferedWriter bufferedWriter = null;
+        try {
+            socket = new Socket("localhost", 8833);
+            inputStreamReader = new InputStreamReader(socket.getInputStream());
+            outputStreamWriter = new OutputStreamWriter(socket.getOutputStream());
+            bufferedReader = new BufferedReader(inputStreamReader);
+            bufferedWriter = new BufferedWriter(outputStreamWriter);
+            while (true) {
+                bufferedWriter.newLine();
+                bufferedWriter.flush();
+                String server = bufferedReader.readLine();
+                if (Objects.equals(server, "FINISH")) break;
+                else System.out.println("SERVER :" + server);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (socket != null) {
+                    socket.close();
+                }
+                if (inputStreamReader != null) {
+                    inputStreamReader.close();
+                }
+                if (outputStreamWriter != null) {
+                    outputStreamWriter.close();
+                }
+                if (bufferedReader != null) {
+                    bufferedWriter.close();
+                }
+                if (bufferedWriter != null) {
+                    bufferedWriter.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
