98e71dbc257970f325584301b89515be1c427f07
AdhamMadkour <64507869+AdhamMadkour@users.noreply.github.com>
2021-11-02 16:42:28 +0200

    Update Client.java

--- Client.java
+++ Client.java
@@ -1,51 +1,28 @@
 import java.io.*;
 import java.net.Socket;
 import java.net.UnknownHostException;
-import java.util.Objects;
 import java.util.Scanner;
 
 public class Client {
-    public static void main(String[] args) {
+    public static void main(String[] arg) {
         Socket socket = null;
-        InputStreamReader inputStreamReader = null;
         OutputStreamWriter outputStreamWriter = null;
-        BufferedReader bufferedReader = null;
         BufferedWriter bufferedWriter = null;
+        Scanner sc = new Scanner(System.in);
         try {
-            socket = new Socket("localhost", 8833);
-            inputStreamReader = new InputStreamReader(socket.getInputStream());
+            socket = new Socket("localhost", 8066);
             outputStreamWriter = new OutputStreamWriter(socket.getOutputStream());
-            bufferedReader = new BufferedReader(inputStreamReader);
             bufferedWriter = new BufferedWriter(outputStreamWriter);
             while (true) {
+                String msg = sc.nextLine();
+                bufferedWriter.write(msg);
                 bufferedWriter.newLine();
                 bufferedWriter.flush();
-                String server = bufferedReader.readLine();
-                if (Objects.equals(server, "FINISH")) break;
-                else System.out.println("SERVER :" + server);
             }
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
-        } finally {
-            try {
-                if (socket != null) {
-                    socket.close();
-                }
-                if (inputStreamReader != null) {
-                    inputStreamReader.close();
-                }
-                if (outputStreamWriter != null) {
-                    outputStreamWriter.close();
-                }
-                if (bufferedReader != null) {
-                    bufferedWriter.close();
-                }
-                if (bufferedWriter != null) {
-                    bufferedWriter.close();
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
         }
     }
 }
