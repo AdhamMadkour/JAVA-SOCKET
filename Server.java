1e21066105d361a58dbf6dd289e9460da2b7fd49
AdhamMadkour <64507869+AdhamMadkour@users.noreply.github.com>
2021-11-02 16:41:49 +0200

    Update Server.java

--- Server.java
+++ Server.java
@@ -4,41 +4,21 @@ import java.net.Socket;
 
 public class Server {
     public static void main(String[] arg) throws IOException {
+        ServerSocket serverSocket = null;
         Socket socket = null;
         InputStreamReader inputStreamReader = null;
-        OutputStreamWriter outputStreamWriter = null;
         BufferedReader bufferedReader = null;
-        BufferedWriter bufferedWriter = null;
-        ServerSocket serverSocket = null;
-
-        serverSocket = new ServerSocket(8833);
+        serverSocket = new ServerSocket(8066);
         while (true) {
             try {
                 socket = serverSocket.accept();
+                System.out.println("Accepted !");
                 inputStreamReader = new InputStreamReader(socket.getInputStream());
-                outputStreamWriter = new OutputStreamWriter(socket.getOutputStream());
                 bufferedReader = new BufferedReader(inputStreamReader);
-                bufferedWriter = new BufferedWriter(outputStreamWriter);
-                int cnt = 1;
                 while (true) {
-                    String msg = bufferedReader.readLine();
-                    bufferedWriter.write("DONE TEST " + cnt);
-                    bufferedWriter.newLine();
-                    bufferedWriter.flush();
-                    ++cnt;
-                    System.out.println(cnt);
-                    if (cnt == 6) {
-                        bufferedWriter.write("FINISH");
-                        bufferedWriter.newLine();
-                        bufferedWriter.flush();
-                        break;
-                    } else continue;
+                    String clientMsg = bufferedReader.readLine();
+                    System.out.println("CLIENT: " + clientMsg);
                 }
-                socket.close();
-                inputStreamReader.close();
-                outputStreamWriter.close();
-                bufferedReader.close();
-                bufferedWriter.close();
             } catch (IOException e) {
                 e.printStackTrace();
             }
